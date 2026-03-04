47696cc8138a4998438219a6bca4f484245d3327
rtblanco <ronnytoribio1@hotmail.com>
2026-03-04 18:44:20 -0500

    created function to check if player won

--- src/main.c
+++ src/main.c
@@ -1,9 +1,39 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int check_winner(int player, int arr[3][3]);
 
 int main() {
+  int matrix[3][3] = {
+    {0,0,0},
+    {0,0,0},
+    {0,0,0},
+  };
+
+
 
   printf("Hello World\n");
   return 0;
+}
+
+int check_winner(int player, int arr[3][3]) {
+  for (int i = 0; i < 3; i++ ) {
+    if (arr[i][0] == player && arr[i][0] == arr[i][1] && arr[i][1] == arr[i][2]) {
+      return 0;
+    }
+
+    if (arr[0][i] == player && arr[0][i] == arr[1][i] && arr[1][i] == arr[2][i]) {
+      return 0;
+    }
+  }
+
+  if (arr[0][0] == player && arr[0][0] == arr[1][1] && arr[1][1] == arr[2][2]) {
+    return 0;
+  }
+
+  if (arr[0][2] == player && arr[0][2] == arr[1][1] && arr[1][1] == arr[2][0]) {
+    return 0;
+  }
+
+  return 1;
 }
